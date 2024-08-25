3bd13d1f790b38964b457d63140517c99add8b5a
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-08-25 17:29:53 -0300

    feat: add monster stats and image to battle card component

--- src/components/monster-battle-card/MonsterBattleCard.extended.tsx
+++ src/components/monster-battle-card/MonsterBattleCard.extended.tsx
@@ -4,6 +4,10 @@ import { Monster } from '../../models/interfaces/monster.interface';
 import {
   BattleMonsterCard,
   BattleMonsterTitle,
+  Image,
+  BattleMonsterName,
+  BattleMonsterStat,
+  ProgressBar,
 } from './MonsterBattleCard.extended.styled';
 
 type MonsterCardProps = {
@@ -11,11 +15,29 @@ type MonsterCardProps = {
   title?: string;
 };
 
-const MonsterBattleCard: React.FC<MonsterCardProps> = ({ title }) => {
+const MonsterBattleCard: React.FC<MonsterCardProps> = ({ title, monster }) => {
+  let cardBody = <BattleMonsterTitle>{title!}</BattleMonsterTitle>;
+
+  if (monster) {
+    cardBody = (
+      <>
+        <Image src={monster.imageUrl} alt={monster.name} />
+        <BattleMonsterName> {monster.name}</BattleMonsterName>
+        <BattleMonsterStat>HP</BattleMonsterStat>
+        <ProgressBar value={monster.hp} variant="determinate"></ProgressBar>
+        <BattleMonsterStat>Attack</BattleMonsterStat>
+        <ProgressBar value={monster.attack} variant="determinate"></ProgressBar>
+        <BattleMonsterStat>Defense</BattleMonsterStat>
+        <ProgressBar
+          value={monster.defense}
+          variant="determinate"></ProgressBar>
+        <BattleMonsterStat>Speed</BattleMonsterStat>
+        <ProgressBar value={monster.speed} variant="determinate"></ProgressBar>
+      </>
+    );
+  }
   return (
-    <BattleMonsterCard centralized>
-      <BattleMonsterTitle>{title!}</BattleMonsterTitle>
-    </BattleMonsterCard>
+    <BattleMonsterCard centralized={!monster}>{cardBody}</BattleMonsterCard>
   );
 };
 
